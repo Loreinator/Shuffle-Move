f02faf55c5d5f401c1bd6c08ff3c942677d76d9e
Loreinator <all4atlantis@gmail.com>
2015-08-17 20:17:53 -0400

    Enabling sorting for stages in the stage chooser
    Stages are now sorted, allowing for a more visually appealing merge of
    old and new configurations.

--- src/shuffle/fwk/data/Stage.java
+++ src/shuffle/fwk/data/Stage.java
@@ -21,23 +21,33 @@ package shuffle.fwk.data;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Queue;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @author Andrew Meyers
  *
  */
-public class Stage {
+public class Stage implements Comparable<Stage> {
    
    public static final int DEFAULT_MOVES = 20;
    public static final int DEFAULT_HEALTH = 10000;
+   
+   private static final Pattern MAIN = Pattern.compile("^\\s*(\\d+)\\s*$");
+   private static final Pattern SPECIAL = Pattern.compile("^\\s*SP[_ ](\\d+)?\\S*\\s*$");
+   private static final Pattern EX = Pattern.compile("^\\s*EX(\\d+)\\s*$");
+   private static final int EX_OFFSET = 10000;
+   private static final int SPECIAL_OFFSET = 100000000;
+   private static final int TYPE_OFFSET = Integer.MAX_VALUE - 1;
 
    private final String stageName;
    private final String targetName;
    private final PkmType targetType;
    private final int stageMoves;
    private final int stageHealth;
-   
    private final String toString;
+   
+   private final int ordering;
 
    public Stage(Stage stage) {
       this(stage.stageName, stage.targetName, stage.targetType, stage.stageMoves, stage.stageHealth);
@@ -52,13 +62,15 @@ public class Stage {
       
       StringBuilder sb = new StringBuilder();
       if (targetType.toString().equals(targetName)) {
-         sb.append(targetName);
+         sb.append(targetName.replaceAll("_", " "));
       } else {
-         sb.append(stageName);
+         sb.append(stageName.replaceAll("_", " "));
          sb.append(": ");
-         sb.append(targetName);
+         sb.append(targetName.replaceAll("_", " "));
       }
       toString = sb.toString();
+      int orderValue = getOrderValue(this);
+      ordering = orderValue;
    }
    
    public Stage(PkmType target) {
@@ -89,6 +101,34 @@ public class Stage {
    public int getHealth() {
       return stageHealth;
    }
+   
+   public static int getOrderValue(Stage s) {
+      String name = s.getName();
+      Matcher main = MAIN.matcher(name);
+      if (main.find()) {
+         return Integer.parseInt(main.group(1));
+      }
+      Matcher special = SPECIAL.matcher(name);
+      if (special.find()) {
+         String value = special.group(1);
+         if (value == null) {
+            return SPECIAL_OFFSET;
+         } else {
+            return SPECIAL_OFFSET + Integer.parseInt(value);
+         }
+      }
+      Matcher ex = EX.matcher(name);
+      if (ex.find()) {
+         String value = ex.group(1);
+         if (value.matches("\\d+")) {
+            return EX_OFFSET + Integer.parseInt(value);
+         }
+      }
+      if (s.getType().toString().equals(name)) {
+         return TYPE_OFFSET;
+      }
+      return Integer.MAX_VALUE;
+   }
 
    @Override
    public String toString() {
@@ -103,5 +143,21 @@ public class Stage {
    @Override
    public boolean equals(Object o) {
       return o != null && o.toString().equals(toString());
+   }
+   
+   /*
+    * (non-Javadoc)
+    * @see java.lang.Comparable#compareTo(java.lang.Object)
+    */
+   @Override
+   public int compareTo(Stage other) {
+      int result = Integer.compare(ordering, other.ordering);
+      if (result == 0) {
+         result = getName().compareTo(other.getName());
+      }
+      if (result == 0) {
+         result = toString().compareTo(other.toString());
+      }
+      return result;
    }
 }
