4f66ca9161106069bc86fbcc2beacf067e65d212
Loreinator <all4atlantis@gmail.com>
2016-03-02 20:44:04 -0500

    Fixed TeamImpl comparrison
    Equals has been implemented

--- src/shuffle/fwk/data/TeamImpl.java
+++ src/shuffle/fwk/data/TeamImpl.java
@@ -248,7 +248,9 @@ public class TeamImpl implements Team {
          return false;
       }
       boolean changed = teamNames.add(s);
-      changed |= setBinding(s, binding);
+      if (changed) {
+         changed |= setBinding(s, binding);
+      }
       return changed;
    }
    
@@ -280,6 +282,19 @@ public class TeamImpl implements Team {
          }
       }
       return sb.toString();
+   }
+   
+   @Override
+   public boolean equals(Object obj) {
+      boolean equal = obj != null && obj instanceof TeamImpl;
+      if (equal) {
+         TeamImpl other = (TeamImpl) obj;
+         equal &= teamNames.equals(other.teamNames);
+         equal &= nameToKey.equals(other.nameToKey);
+         equal &= keyToName.equals(other.keyToName);
+         equal &= megaSlotName == other.megaSlotName || megaSlotName != null && megaSlotName.equals(other.megaSlotName);
+      }
+      return equal;
    }
    
    @Override
